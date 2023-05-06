86a51c3362b7d571bd05715a75e09572262a4f8a
Giovanni Brunno Previatti <gpreviatt@gmail.com>
2023-05-06 14:30:11 -0300

    add event hub samples

--- Readme.md
+++ Readme.md
@@ -1,23 +1,17 @@
 # Azure messaging tools samples
 
+Basic project with implementation sample projects provides by Microsoft documentation of common messaging tools of azure
 
 ## Service Bus
 
-### Queue Send
+### Queue
 
+[Link reference](https://learn.microsoft.com/en-us/azure/service-bus-messaging/service-bus-dotnet-get-started-with-queues?tabs=connection-string)
 
-### Queue Listen
+### Topic
 
-
-### Topic Send
-
-
-### Topic Listen
-
+[Link reference](https://learn.microsoft.com/en-us/azure/service-bus-messaging/service-bus-dotnet-how-to-use-topics-subscriptions?tabs=connection-string)
 
 ## Event Hub
 
-### Send
-
-
-### Listen
+[Link reference](https://learn.microsoft.com/en-us/azure/event-hubs/event-hubs-dotnet-standard-getstarted-send?tabs=connection-string%2Croles-azure-portal)
---
+++ src/EventHub.Send/Program.cs
@@ -0,0 +1,33 @@
+﻿using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using System.Text;
+
+// number of events to be sent to the event hub
+var numOfEvents = 3;
+
+// The Event Hubs client types are safe to cache and use as a singleton for the lifetime
+// of the application, which is best practice when events are being published or read regularly.
+var producerClient = new EventHubProducerClient("");
+
+// Create a batch of events 
+using var eventBatch = await producerClient.CreateBatchAsync();
+
+for (var i = 1; i <= numOfEvents; i++)
+{
+    if (!eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes($"Event {i}"))))
+    {
+        // if it is too large for the batch
+        throw new Exception($"Event {i} is too large for the batch and cannot be sent.");
+    }
+}
+
+try
+{
+    // Use the producer client to send the batch of events to the event hub
+    await producerClient.SendAsync(eventBatch);
+    Console.WriteLine($"A batch of {numOfEvents} events has been published.");
+}
+finally
+{
+    await producerClient.DisposeAsync();
+}
